c28ddd29a29d900af3c5cbe04254c92d34c9fbb2
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-17 00:41:48 +0530

    d:32: Plotted Fake data for now.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # What is this project about?
+
+This is a web app which shows weather data 
+about a certain number of days for a 
+particular place in Europe.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 # Put dynamic things into variable (Rules)
@@ -10,4 +11,12 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       options=("Temperature", "Sky"))
 graph_label = f"{option} for the next {days} days in {place}."
-st.title(graph_label)
+# use f11 to bookmark it.
+st.subheader(graph_label)
+
+dates = ["2022-12-10", "2022-12-11", "2022-12-12"]
+temperatures = [10, 11, 20]
+# x and y take list of arrays.
+figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperatures (C}"})
+# It will take the a figure created by plotly.
+st.plotly_chart(figure)
